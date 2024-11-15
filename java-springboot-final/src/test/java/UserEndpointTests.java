c3377216ff480f6ea850a5435e2eae54f6120f88
Michael Lambert <mlambert125@live.com>
2024-11-15 00:37:20 -0500

    Adding github actions

--- java-springboot-final/src/test/java/UserEndpointTests.java
+++ java-springboot-final/src/test/java/UserEndpointTests.java
@@ -1,5 +1,6 @@
 import org.example.SpringBootApplication;
 import org.example.models.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -20,6 +21,7 @@ public class UserEndpointTests extends WebStoreTest {
      * Tests that getting users fails if not authorized.
      */
     @Test
+    @DisplayName("GET /api/users should return a 401 if not authorized")
     public void getUsersShouldFailIfNotAuthorized() {
         var result = this.restTemplate.getForEntity(getBaseUrl() + "/api/users", String.class);
         var responseCode = result.getStatusCode();
@@ -27,6 +29,7 @@ public class UserEndpointTests extends WebStoreTest {
     }
 
     @Test
+    @DisplayName("GET /api/users should return a 403 if not an admin")
     public void getUsersShouldFailIfUserNotAdmin() throws SQLException {
         jdbcTemplate.update(
             "insert into users (username, password) values ('user', ?);",
@@ -42,6 +45,7 @@ public class UserEndpointTests extends WebStoreTest {
      * Tests that getting users succeeds if authorized.
      */
     @Test
+    @DisplayName("GET /api/users should return a 200 and a list of one user if authorized")
     public void getUserShouldSucceedIfAuthorized() {
         var requestEntity = GetAuthEntity("test-admin", "admin");
         var result = this.restTemplate.exchange(getBaseUrl() + "/api/users", HttpMethod.GET, requestEntity, User[].class);
